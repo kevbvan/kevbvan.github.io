cf1e8a72b944743bb93a003b7d4dd4fdecbb50ea
Kevin Van <kev.b.van@gmail.com>
2024-08-09 15:29:51 -0700

    add base functionality to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Website Loaded');
 });
+
+// Highlight Page on Navbar
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header .navbar li a');
+
+window.onscroll= () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.header .navbar li a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+} 
+
